844cff6c94e0d8b1ccd15906eb79a2b037924f74
stephenmcd <steve@jupo.org>
2011-10-25 18:30:58 +1100

    Added setup files.

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup
+
+import babbler
+
+
+setup(
+    name="babbler",
+    version=babbler.__version__,
+    author="Stephen McDonald",
+    author_email="stephen.mc@gmail.com",
+    description=babbler.__doc__.replace("\n", " ").strip(),
+    long_description=open("README.rst").read(),
+    license="BSD",
+    url="http://github.com/stephenmcd/babbler/",
+    py_modules=["babbler"],
+    install_requires=[r.strip() for r in open("requirements.txt") if r],
+    entry_points="""
+        [console_scripts]
+        babbler=babbler:main
+    """,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop,"
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Communications",
+        "Topic :: Text Processing",
+    ]
+)
